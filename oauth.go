d7eecc926614e03021f04279d5520b5fb40df979
Supratik Das <rick.das08@gmail.com>
2019-03-26 22:19:57 +0530

    update oauth.go

--- oauth.go
+++ oauth.go
@@ -1,12 +1,18 @@
 package falconClientGolang
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
-	"golang.org/x/oauth2"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
+
+type DataResponse struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	ExpiresIn   int    `json:"expires_in"`
+}
 
 type falconClientGolang struct {
 	falconClientId         string
@@ -16,130 +22,101 @@ type falconClientGolang struct {
 	falconAccountsUrl      string
 }
 
-func New(falconClientId, falconClientSecret, falconUrlAccessToken, falconUrlResourceOwner, falconAccountsUrl string) (falconClientGolang, *oauth2.Config) {
+func New(falconClientId, falconClientSecret, falconUrlAccessToken, falconUrlResourceOwner, falconAccountsUrl string) falconClientGolang {
 	config := falconClientGolang{falconClientId, falconClientSecret, falconUrlAccessToken, falconUrlResourceOwner, falconAccountsUrl}
-
-	var credentials = &oauth2.Config{
-		RedirectURL:  "",
-		ClientID:     falconClientId,
-		ClientSecret: falconClientSecret,
-		Scopes:       []string{"email", "image_url", "organization"},
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  "https://provider.com/o/oauth2/auth",
-			TokenURL: "http://falcon.sdslabs.co/access_token",
-		},
-	}
-
-	return config, credentials
+	return config
 }
 
-// func parseJSON(path string) (map[string]interface{}, error) {
-// 	var config map[string]interface{}
-// 	file, err := ioutil.ReadFile(path)
-// 	if err != nil {
-// 		return config, err
-// 	}
-// 	err = json.Unmarshal(file, &config)
-// 	if err != nil {
-// 		return config, err
-// 	}
-// 	return config, nil
-// }
-
-// var config, _ = parseJSON("config.json")
-
-// var falconClientGolang.falconUrlResourceOwner string = config["falcon_url_resource_owner_details"].(string)
-// var falconClientGolang.falconAccountsUrl string = config["falcon_falconClientGolang.falconAccountsUrl"].(string)
 var COOKIE_NAME string = "sdslabs"
 
-func makeRequest(url string, token *oauth2.Token) ([]byte, error) {
+func makeRequest(url, token string) (string, error) {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return nil, fmt.Errorf("error setting up a request: %s", err.Error())
+		return "", fmt.Errorf("error setting up a request: %s", err.Error())
 	}
 
-	req.Header.Set("Authorization", "Bearer "+token.AccessToken)
+	req.Header.Set("Authorization", "Bearer "+token)
 	req.Header.Set("Content-Type", "application/json")
 
 	response, err := client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("error making a request: %s", err.Error())
+		return "", fmt.Errorf("error making a request: %s", err.Error())
 	}
 
 	defer response.Body.Close()
 	contents, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return nil, fmt.Errorf("failed read response: %s", err.Error())
+		return "", fmt.Errorf("failed read response: %s", err.Error())
 	}
-	return contents, nil
+	return string(contents), nil
 }
 
-func GetUserById(id string, config falconClientGolang, credentials *oauth2.Config) ([]byte, error) {
-	token, err := credentials.Exchange(context.Background(), id)
-	if err != nil {
-		return nil, fmt.Errorf("code exchange wrong: %s", err.Error())
-	}
+func getToken(config falconClientGolang) string {
+	payload := strings.NewReader("client_id=" + config.falconClientId + "&client_secret=" + config.falconClientSecret + "&grant_type=client_credentials")
+	req, _ := http.NewRequest("POST", config.falconUrlAccessToken, payload)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
-	user_data, err := makeRequest(config.falconUrlResourceOwner+`/`+id, token)
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	response := &DataResponse{}
+	json.Unmarshal([]byte(string(body)), &response)
+
+	return string(response.AccessToken)
+}
+
+func GetUserById(id string, config falconClientGolang) (string, error) {
+	token := getToken(config)
+	user_data, err := makeRequest(config.falconUrlResourceOwner+"id/"+id, token)
 	if err != nil {
-		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
+		return "", fmt.Errorf("failed getting user info: %s", err.Error())
 	}
 	return user_data, nil
 }
 
-func GetUserByUsername(username string, config falconClientGolang, credentials *oauth2.Config) ([]byte, error) {
-	token, err := credentials.Exchange(context.Background(), username)
+func GetUserByUsername(username string, config falconClientGolang) (string, error) {
+	token := getToken(config)
+	user_data, err := makeRequest(config.falconUrlResourceOwner+"username/"+username, token)
 	if err != nil {
-		return nil, fmt.Errorf("code exchange wrong: %s", err.Error())
-	}
-
-	user_data, err := makeRequest(config.falconUrlResourceOwner+`/username/`+username, token)
-	if err != nil {
-		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
+		return "", fmt.Errorf("failed getting user info: %s", err.Error())
 	}
 	return user_data, nil
 }
 
-func GetUserByEmail(email string, config falconClientGolang, credentials *oauth2.Config) ([]byte, error) {
-	token, err := credentials.Exchange(context.Background(), email)
+func GetUserByEmail(email string, config falconClientGolang) (string, error) {
+	token := getToken(config)
+	user_data, err := makeRequest(config.falconUrlResourceOwner+"email/"+email, token)
 	if err != nil {
-		return nil, fmt.Errorf("code exchange wrong: %s", err.Error())
-	}
-
-	user_data, err := makeRequest(config.falconUrlResourceOwner+`/email/`+email, token)
-	if err != nil {
-		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
+		return "", fmt.Errorf("failed getting user info: %s", err.Error())
 	}
 	return user_data, nil
 }
 
-func GetLoggedInUser(cookies map[string][]string, config falconClientGolang, credentials *oauth2.Config) ([]byte, error) {
+func GetLoggedInUser(config falconClientGolang, r *http.Request) (string, error) {
 	// hash := cookies[COOKIE_NAME]
+	// hash, _ := r.Cookie(COOKIE_NAME)
+	// fmt.Fprint(w, cookie)
 	var hash string = ""
 	if hash == "" {
-		return nil, fmt.Errorf("cookie not found")
+		return "", fmt.Errorf("cookie not found")
 	}
-
-	token, err := credentials.Exchange(context.Background(), hash)
-	if err != nil {
-		return nil, fmt.Errorf("code exchange wrong: %s", err.Error())
-	}
-
+	token := getToken(config)
 	user_data, err := makeRequest(config.falconUrlResourceOwner+`/logged_in_user/`+hash, token)
 	if err != nil {
-		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
+		return "", fmt.Errorf("failed getting user info: %s", err.Error())
 	}
 	return user_data, nil
 }
 
-func Login(cookies map[string][]string, config falconClientGolang, credentials *oauth2.Config, w http.ResponseWriter, r *http.Request) ([]byte, error) {
-	user_data, err := GetLoggedInUser(cookies, config, credentials)
+func Login(config falconClientGolang, w http.ResponseWriter, r *http.Request) (string, error) {
+	user_data, err := GetLoggedInUser(config, r)
 	if err != nil {
-		return nil, fmt.Errorf("failed to login with given credentials: %s", err.Error())
+		return "", fmt.Errorf("failed to login with given credentials: %s", err.Error())
 	}
 
-	if user_data == nil {
+	if user_data == "" {
 		http.Redirect(w, r, config.falconAccountsUrl+`/login?redirect=//`, http.StatusTemporaryRedirect)
 	}
 	return user_data, nil
