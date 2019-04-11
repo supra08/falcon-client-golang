cba031f52c960842daa216d6197f9149e3ab2229
Supratik Das <rick.das08@gmail.com>
2019-04-11 20:16:04 +0530

    modify oauth

--- oauth.go
+++ oauth.go
@@ -99,6 +99,7 @@ func GetLoggedInUser(config falconClientGolang, cookie string) (string, error) {
 	// hash, _ := r.Cookie(COOKIE_NAME)
 	// fmt.Fprint(w, cookie)
 	fmt.Println(cookie)
+	fmt.Println("Woah I printed this after the cookie")
 	var hash string = strings.Split(cookie, "=")[1].(string)
 	fmt.Println(hash)
 	if hash == "" {
